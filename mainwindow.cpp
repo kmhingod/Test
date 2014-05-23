ca4694756571e2256f7ea72eaf5d7b60d94a4553
jefric-minhyoungkim <jefric.mh.kim@gmail.com>
2014-05-23 21:19:08 +0900

    in mainwindow.cpp write some my message and readme2 noted about git
    Change-Id: If101348724d67412506c4d551797249f396bd260

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+/*
+ This is a mainwindow.cpp file
+ I can do it!!!
+*/
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
